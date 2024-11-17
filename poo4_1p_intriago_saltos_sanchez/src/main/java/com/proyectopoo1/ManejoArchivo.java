160cc91dd5a7f20631224d2c51663897b474f305
DavoLS-24 <davidsaltos799@gmail.com>
2024-11-17 12:20:30 -0500

    creación y prueba de la Clase ManejoArchivo

---
+++ poo4_1p_intriago_saltos_sanchez/src/main/java/com/proyectopoo1/ManejoArchivo.java
@@ -0,0 +1,76 @@
+package com.proyectopoo1;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+
+public class ManejoArchivo {
+
+    // Método para lectura de archivos
+     public static ArrayList<String> leerArchivo(String ruta) {
+        ArrayList<String> lineas = new ArrayList<>();
+        File archivo = null;
+        FileReader fr = null;
+        BufferedReader br = null;
+
+        try {
+            archivo = new File(ruta);  
+            fr = new FileReader(archivo,StandardCharsets.UTF_8);
+            br = new BufferedReader(fr);
+
+ 
+            String linea;
+            while ((linea = br.readLine()) != null) {
+                System.out.println(linea);
+                lineas.add(linea);
+
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace(System.out);
+        } finally {
+
+            try {
+                if (null != fr) {
+                    fr.close();
+                }
+            } catch (IOException e2) {
+                e2.printStackTrace(System.out);
+            }
+        }
+        return lineas;
+    }
+
+    public static void escribirArchivo(String nombreArchivo, ArrayList<String> datosAEscr) {
+
+        FileWriter fichero = null;
+        BufferedWriter bw = null;
+      
+        try {
+            fichero = new FileWriter(nombreArchivo);
+            bw = new BufferedWriter(fichero);
+            for(String linea: datosAEscr){
+                bw.write(linea+"\n");
+            }
+            System.out.println("ksdsdlsd");
+
+        } catch (IOException e) {
+            e.printStackTrace(System.out);
+        } finally {
+            try {
+
+                if (null != fichero) {
+                    bw.close();
+                }
+            } catch (IOException e2) {
+                e2.printStackTrace(System.out);
+            }
+        }
+    }
+
+}
